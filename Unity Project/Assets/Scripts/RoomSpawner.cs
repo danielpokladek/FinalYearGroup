13f2e4466e3204dfa00d9d26364209de760db242
Daniel Pokladek <danielpokladek@gmail.com>
2018-08-09 16:50:01 +0100

    Random generation

---
+++ Unity Project/Assets/Scripts/RoomSpawner.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour {
+    
+    public int openingDirection;
+    // 1 --> need bottom door
+    // 2 --> need top door
+    // 3 --> need left door
+    // 4 --> need right door
+
+    private RoomTemplates templates;
+    private int rand;
+    private bool spawned = false;
+
+    private void Start()
+    {
+        templates = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomTemplates>();
+        Invoke("Spawn", 0.1f);
+    }
+
+    void Spawn () {
+        if (spawned == false)
+        {
+            if (openingDirection == 1)
+            {
+                // Need a room with BOTTOM door.
+                rand = Random.Range(0, templates.bottomRooms.Length -1);
+                Instantiate(templates.bottomRooms[rand], transform.position, templates.bottomRooms[rand].transform.rotation);
+            }
+            else if (openingDirection == 2)
+            {
+                // Need a room with TOP door.
+                rand = Random.Range(0, templates.topRooms.Length -1);
+                Instantiate(templates.topRooms[rand], transform.position, templates.topRooms[rand].transform.rotation);
+            }
+            else if (openingDirection == 3)
+            {
+                // Need a room with LEFT door.
+                rand = Random.Range(0, templates.leftRooms.Length -1);
+                Instantiate(templates.leftRooms[rand], transform.position, templates.leftRooms[rand].transform.rotation);
+            }
+            else if (openingDirection == 4)
+            {
+                // Need a room with RIGHT door.
+                rand = Random.Range(0, templates.rightRooms.Length -1);
+                Instantiate(templates.rightRooms[rand], transform.position, templates.rightRooms[rand].transform.rotation);
+            }
+
+            spawned = true;
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("SpawnPoint") && other.GetComponent<RoomSpawner>().spawned == true)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
